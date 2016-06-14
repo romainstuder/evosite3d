31e7b5a3b35e78343a3643d9f744145eb77bc0b3
romainstuder <rstuder@ebi.ac.uk>
2016-06-14 10:00:54 +0100

    Script to translate CDS to AA using Biopython

---
+++ translate_dna.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import sys
+from Bio import SeqIO
+
+handle = open(sys.argv[1], "rU")
+for record in SeqIO.parse(handle, "fasta"):
+    print ">"+record.id
+    print record.seq.translate(to_stop=True)
+handle.close()
