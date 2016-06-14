283af22cdc84b2eed39b51a0e11039ea0b6f1154
romainstuder <rstuder@ebi.ac.uk>
2016-06-14 10:31:41 +0100

    Extract sequences from FASTA file

---
+++ extract_sequences.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import sys
+from Bio import SeqIO
+
+# Load list of sequences to keep
+sequence_list = []
+file_in = open(sys.argv[1], "r")
+while 1:
+    line = file_in.readline()
+    if line == "":
+        break
+    tab = line.split()
+    gene = tab[0]
+    sequence_list.append(gene)
+file_in.close()
+
+# Write only sequences that are in the list to keep 
+for record in SeqIO.parse(open(sys.argv[2], "rU"), "fasta"):
+    gene = record.id
+    if gene in sequence_list:
+        sequence = str(record.seq)
+        print ">"+gene
+        print sequence
+
+
