2b853b983fd8cd7f9b82a0b6afe9ff54ec441e4c
rstuder <evosite3d@protonmail.com>
2021-10-03 17:51:11 +0100

    Upper case

--- extract_seq_prob.py
+++ extract_seq_prob.py
@@ -4,8 +4,9 @@
 
 import sys
 
-tag = 0
-branch = ""
+TAG = 0
+BRANCH = ""
+
 file_in = open(sys.argv[1], "r")
 while 1:
     line = file_in.readline()
@@ -13,8 +14,8 @@ while 1:
         break
     line = line.rstrip()
     if "Prob distribution at node 210, by site" in line:
-        tag = 0
-    if tag == 1:
+        TAG = 0
+    if TAG == 1:
         tab = line.split()
         if len(tab) > 3:
             # print(tab)
@@ -23,6 +24,6 @@ while 1:
             print(site, prob)
             print(len(prob))
     if "Prob distribution at node 209, by site" in line:
-        tag = 1
+        TAG = 1
 
 file_in.close()
